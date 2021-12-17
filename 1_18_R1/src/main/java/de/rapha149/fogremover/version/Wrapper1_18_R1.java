b48c03257df63a34505fe5e076951b301fa494b1
Rapha149 <rapha.2626.149@gmail.com>
2021-12-17 10:25:59 +0100

    Add methods for individual player distances.

--- 1_18_R1/src/main/java/de/rapha149/fogremover/version/Wrapper1_18_R1.java
+++ 1_18_R1/src/main/java/de/rapha149/fogremover/version/Wrapper1_18_R1.java
@@ -4,12 +4,14 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelPipeline;
 import net.minecraft.network.protocol.game.PacketPlayOutLogin;
+import net.minecraft.network.protocol.login.PacketLoginInStart;
 import net.minecraft.server.network.ServerConnection;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_18_R1.CraftServer;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.UUID;
 
 public class Wrapper1_18_R1 implements VersionWrapper {
 
@@ -24,17 +26,30 @@ public class Wrapper1_18_R1 implements VersionWrapper {
     }
 
     @Override
-    public Class<?> getPacketClass() {
+    public Class<?> getLoginPacketClass() {
         return PacketPlayOutLogin.class;
+    }
+
+    @Override
+    public Class<?> getLoginStartPacketClass() {
+        return PacketLoginInStart.class;
     }
 
     @Override
     public Object replaceViewDistance(Object obj, int viewDistance) {
         if(!(obj instanceof PacketPlayOutLogin packet))
-            throw new IllegalArgumentException("Object not instance of class " + getPacketClass().getName());
+            throw new IllegalArgumentException("Object not instance of class " + getLoginPacketClass().getName());
 
         return new PacketPlayOutLogin(packet.b(), packet.c(), packet.d(), packet.e(), packet.f(), packet.g(),
                 packet.h(), packet.i(), packet.j(), packet.k(), viewDistance, packet.m(), packet.n(), packet.o(),
                 packet.p(), packet.q());
+    }
+
+    @Override
+    public UUID getUUID(Object obj) {
+        if(!(obj instanceof PacketLoginInStart packet))
+            throw new IllegalStateException("Object not instance of class " + getLoginStartPacketClass().getName());
+
+        return packet.b().getId();
     }
 }
--- abstraction/src/main/java/de/rapha149/fogremover/version/VersionWrapper.java
+++ abstraction/src/main/java/de/rapha149/fogremover/version/VersionWrapper.java
@@ -3,12 +3,17 @@ package de.rapha149.fogremover.version;
 import io.netty.channel.ChannelPipeline;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface VersionWrapper {
 
     List<ChannelPipeline> getServerPipelines() throws NoSuchFieldException, IllegalAccessException;
 
-    Class<?> getPacketClass();
+    Class<?> getLoginPacketClass();
+
+    Class<?> getLoginStartPacketClass();
 
     Object replaceViewDistance(Object obj, int viewDistance);
+
+    UUID getUUID(Object obj);
 }
