d93e38989435370de16a65ea06ab7e7cb658ac24
Rapha149 <49787110+Rapha149@users.noreply.github.com>
2025-07-12 15:20:10 +0200

    Add two very small changes.

--- plugin/src/main/java/de/rapha149/clearfog/ClearFog.java
+++ plugin/src/main/java/de/rapha149/clearfog/ClearFog.java
@@ -27,6 +27,7 @@ public final class ClearFog extends JavaPlugin {
             "1.21.3", "1_21_R2",
             "1.21.4", "1_21_R3",
             "1.21.5", "1_21_R4",
+            "1.21.6", "1_21_R5",
             "1.21.7", "1_21_R5"
     );
     private static final String NEWEST_VERSION = "1_21_R5";
